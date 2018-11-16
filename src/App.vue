e1fd80986975c792c9b9765c8d96b59b3f1f4ca6
csc9034 <csc9034@naver.com>
2018-11-16 18:06:18 +0900

    git initial commit:

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="app">
+
+    <div id="sidebar-wrapper">
+      <h3>Side Menu</h3>
+      <ul class="sidebar-nav">
+        <navBar/>
+      </ul>
+    </div>
+
+    <div slot="page-content-wrapper" id="page-content-wrapper">
+      <img src="./assets/logo.png">
+      <h1>Brett X Vue</h1>
+      <router-view/>
+    </div>
+  </div>
+  
+</template>
+
+<script>
+import navBar from './components/common/NavBar';
+export default {
+  name: 'App',
+  components: {
+    navBar
+  }
+}
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  #header {
+    width: 100%;
+    padding: 20px;
+    font-size: x-large;
+  }
+
+  #sidebar-wrapper {
+    padding-top: 20px;
+    display: table-cell;
+    vertical-align: middle;
+    position: fixed;
+    width: 170px;
+    height: 100%;
+    overflow-x: hidden;
+    overflow-y: auto;
+  }
+
+  .sidebar-nav {
+    width: 170px;
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+
+  #page-content-wrapper {
+    padding: 20px;
+    margin-left: 170px;
+  }
+</style>
---
+++ src/components/common/NavBar.vue
@@ -0,0 +1,38 @@
+<template>
+<nav class="app-nav">
+    <router-link to="/" class="bar-item">
+      <p>시작하기</p>
+    </router-link>
+    <router-link to="/component" class="bar-item">
+      <p>컴포넌트</p>
+    </router-link>
+    <router-link to="/template-layout" class="bar-item">
+      <p>템플릿 문법</p>
+    </router-link>
+    <router-link to="/computed-and-watch-layout" class="bar-item">
+      <p>계산된 속성과 감시자</p>
+    </router-link>
+    <router-link to="/class-and-bind-layout" class="bar-item">
+      <p>클래스와 스타일 바인딩</p>
+    </router-link>
+    <router-link to="/conditional-rendering-layout" class="bar-item">
+      <p>조건부 렌더링</p>
+    </router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'nav-bar'
+};
+</script>
+
+<style>
+  .app-nav {
+
+  }
+
+  .bar-item p {
+    font-size: medium;
+  }
+</style>
